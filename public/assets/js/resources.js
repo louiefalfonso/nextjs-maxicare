ffdc931a01b58e8adc8c5a08a7d3e0399d09cd9b
Louie Alfonso <lfriasalfonso@gmail.com>
2024-07-02 19:42:13 +0100

    Update Resouces File

--- public/assets/js/resources.js
+++ public/assets/js/resources.js
@@ -1,8 +1,1 @@
 import "@/assets/js/main.js";
-import "@/assets/js/vendor/jquery-3.7.1.min.js";
-import "@/assets/js/plugins/waypoints.min.js";
-import "@/assets/js/vendor/bootstrap.bundle.min.js";
-import "@/assets/js/plugins/meanmenu.min.js";
-import "@/assets/js/plugins/odometer.min.js";
-import "@/assets/js/plugins/swiper.min.js";
-import "@/assets/js/plugins/wow.js";
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "@/assets/js/resources";
-import "./globals.css";
+import "@/assets/css/resources.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
